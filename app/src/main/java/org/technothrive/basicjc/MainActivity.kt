4f8a8f72044f0edcbaeadb5f9f56a2cbd71515f4
aiyappa <aiyappa@digitalgreen.org>
2025-04-13 10:53:49 +0530

    first commit

---
+++ app/src/main/java/org/technothrive/basicjc/MainActivity.kt
@@ -0,0 +1,58 @@
+package org.technothrive.basicjc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpleText()
+        }
+    }
+}
+
+
+@Composable
+fun SimpleText() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Hello Welcome to Android App Development Using Jetpack Compose",
+            textAlign = TextAlign.Center,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.Center)
+        )
+
+        Text(
+            text = "Technothrive",
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp)
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun SimpleTextPreview() {
+    SimpleText()
+}
